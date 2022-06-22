fabab9a12c55368d994c98e1006c9c9cce137adb
Ahadjon Ubaydullayev <ahadjon.ubaydullayev16@gmail.com>
2022-06-22 10:48:17 -0400

    last changes

--- smart_english/register/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from register.views import *
-
-urlpatterns = [
-    path('list-config/', list_config, name="list-config"),
-    path('student/', students, name="students"),
-    path('building/', building, name="building"), # assigned path should be fixed
-
-]
--- smart_english/register/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-from settings.views import *
-
-def list_config(request):
-    return render(request, 'register/list-config.html')
-
-
